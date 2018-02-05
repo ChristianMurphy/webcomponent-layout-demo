b53aec60efdea185ca48fb54d1778a6c2ca0bbbf
Christian Murphy <cmurphy@unicon.net>
2018-02-04 21:49:22 -0700

    feat: add vue hackernews component

---
+++ vue/news.js
@@ -0,0 +1,45 @@
+import Vue from '/node_modules/vue/dist/vue.runtime.esm.js';
+import vueCustomElement from '/node_modules/vue-custom-element/dist/vue-custom-element.esm.js';
+
+Vue.use(vueCustomElement);
+
+Vue.customElement(
+  'vue-news',
+  {
+    data() {
+      return {
+        items: []
+      };
+    },
+    async created() {
+      const response = await fetch(
+        'https://hacker-news.firebaseio.com/v0/topstories.json'
+      );
+      const topStoryIds = await response.json();
+      const topTenStoryIds = topStoryIds.slice(0, 10);
+      this.items = await Promise.all(
+        topTenStoryIds.map(async id => {
+          const response = await fetch(
+            `https://hacker-news.firebaseio.com/v0/item/${id}.json`
+          );
+          return await response.json();
+        })
+      );
+    },
+    render(createElement) {
+      return createElement('section', {}, [
+        createElement('h1', {}, 'Vue HackerNews'),
+        createElement(
+          'ol',
+          {},
+          this.items.map(item =>
+            createElement('li', {}, [
+              createElement('a', { href: item.url }, item.title)
+            ])
+          )
+        )
+      ]);
+    }
+  },
+  { shadow: true }
+);
